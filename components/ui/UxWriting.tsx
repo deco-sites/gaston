221a384e2057809dedacc0b43445676169cd9ea2
pandeiro7 <rafasoli10@gmail.com>
2024-03-15 20:25:01 -0300

    feat: ux writing

---
+++ components/ui/UxWriting.tsx
@@ -0,0 +1,68 @@
+import type { SectionProps } from "deco/types.ts";
+
+export interface UxWriting {
+  /** @description RegExp to enable this banner on the current URL. Use /feminino/* to display this banner on feminino category  */
+  matcher: string;
+  title: string;
+  markupTitle?: boolean;
+  description: string;
+}
+
+const DEFAULT_PROPS = {
+  title: "Maculino",
+  markupTitle: false,
+  matcher: "/*",
+  description:
+    "Nulla vitae malesuada risus. Nam accumsan varius elementum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed nec quam quis nulla vehicula malesuada. Suspendisse sit amet lacus laoreet, interdum urna id, maximus arcu. Aliquam egestas porta ex, et tempus libero tempor vel. Morbi massa orci, faucibus nec venenatis in, cursus sit amet nibh. Ut sit amet metus libero.",
+};
+
+export interface Props {
+  seoTexts?: UxWriting[];
+}
+
+function UxWriting(props: SectionProps<ReturnType<typeof loader>>) {
+  const { seoText } = props;
+
+  if (!seoText) {
+    return null;
+  }
+
+  const { title, markupTitle, description } = seoText;
+
+  return (
+    <>
+      <div class="w-full pt-6 pb-16 bg-base-300">
+        <div
+          class={`w-11/12 max-w-[1300px] mx-auto bg-base-100 rounded-3xl flex flex-col justify-center items-center gap-2.5 py-6 px-5 lg:py-8 lg:px-8 2xl:px-[124px] border border-black border-opacity-10`}
+        >
+          <h2
+            class={`${
+              markupTitle
+                ? "bg-primary p-1 text-base-100"
+                : "text-primary-content"
+            } text-center italic font-bold text-2xl leading-7`}
+          >
+            {title}
+          </h2>
+          <p
+            class={`text-sm leading-[18px] text-primary-content font-normal text-center`}
+          >
+            {description}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const loader = (props: Props, req: Request) => {
+  const { seoTexts } = { ...DEFAULT_PROPS, ...props };
+
+  const seoText = seoTexts?.find(({ matcher }) =>
+    new URLPattern({ pathname: matcher }).test(req.url)
+  );
+
+  return { seoText };
+};
+
+export default UxWriting;
